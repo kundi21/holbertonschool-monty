eb4b92e33803581bf08dd9ee67e85bf1b40ba2e4
GabiAcosta <acostagabriel1311@gmail.com>
2023-05-11 09:51:39 -0700

    implemented check_num function

--- opcodes.c
+++ opcodes.c
@@ -6,5 +6,33 @@
  */
 void _nop(stack_t **stack, unsigned int line_number)
 {
-    (void) stack, (void) line_number;
+	(void) stack, (void) line_number;
+}
+
+void _push(stack_t **stack, unsigned int line_number)
+{
+	int num;
+	num = atoi(strtok(NULL, ' \t'));
+	stack_t *node;
+
+
+}
+
+int check_num(char *num)
+{
+	int length = strlen(num);
+	int i = 0;
+	int check;
+
+	if (!num)
+		return (0);
+	for (; i < length; i++)
+	{
+		if (check = isdigit(num[i]))
+			if (check = 0)
+				return (check);
+			else
+				check = 1;
+	}
+	return (check);
 }
